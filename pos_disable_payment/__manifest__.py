3af0a60f5df6084ad5ef8893f93926b594eef6b2
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-06-07 08:23:19 +0200

    '

---
+++ pos_disable_payment/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Disable payments in POS",
+    'summary': "Control access to the POS payments",
+    'version': '10.0.0.1',
+    'author': 'IT-Projects LLC, Ivan Yelizariev,DVIT.ME',
+    'license': 'LGPL-3',
+    'category': 'Point Of Sale',
+    "support": "apps@it-projects.info",
+    'website': 'http://dvit.me',
+    'depends': ['point_of_sale'],
+    'images': ['images/pos_payment_access.png'],
+    "price": 40.00,
+    "currency": "EUR",
+    'data': [
+        'views.xml',
+    ],
+    'installable': True,
+}
