a4d6bda6b11787f315b5eb62ae949c1fedcb9414
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-09-25 10:26:20 +0200

    Move hr_payroll_commission to dvit-odoo repo and rename to dvit_hr_payroll_commission due to restructuring and some logic changes.

--- hr_payroll_commission/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Coninckx David (Open Net Sarl)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import hr_contract
-from . import hr_payslip
-from . import account_invoice
-from . import account_move_line
